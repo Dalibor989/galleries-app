51b2cd70448eeb1c7bc69cf3592104f91131b3ef
Dalibor989 <lazic_dalibor@hotmail.com>
2021-09-22 16:23:04 +0200

    small corrections.

--- src/containers/ViewGallery.js
+++ src/containers/ViewGallery.js
@@ -1,10 +1,13 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import galleriesService from "../services/GalleriesService";
+import useFormattedDate from "../hooks/useFormattedDate";
 
 function ViewGallery() {
   const [gallery, setGallery] = useState([]);
   const { id } = useParams()
+
+  const dateFormat = useFormattedDate(gallery.created_at);
 
   useEffect(() => {
     const fetchGallery = async () => {
@@ -26,14 +29,11 @@ function ViewGallery() {
 
       {gallery.images || gallery.user ? <p>{gallery.user.firstName} {gallery.user.lastName}</p> : ""}
     
+      <p>{dateFormat}</p>
 
       {gallery.images && gallery.images.length ? gallery.images.map((image) => (
         <img key={image.id} src={image.imageUrl}/> 
       )) : ""} 
-
-      {/* {gallery.images && gallery.images.length ?
-      <img src={gallery.images.length ? gallery.images[0].imageUrl : ''}/> : "No image"
-      } */}
     </div>
   )
 }
